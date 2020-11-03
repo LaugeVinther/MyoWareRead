a5c4dac2407c7401bfc4f06b875f16399d378b9b
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2020-11-03 12:21:39 +0100

    Opdaterede programmet til at skrive i CSV-fil

--- MyoWareRead/MyoWareRead.py
+++ MyoWareRead/MyoWareRead.py
@@ -1,15 +1,44 @@
+import time
+from time import sleep
+from datetime import datetime
 import board
 import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+import keyboard
+
+# Create the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
 
-import adafruit_ads1x15.ads1015 as ADS
-
-from adafruit_ads1x15.analog_in import AnalogIn
-
+# Create the ADC object using the I2C bus
 ads = ADS.ADS1015(i2c)
 
+# Create single-ended input on channel 0
 chan = AnalogIn(ads, ADS.P0)
 
-print(chan.value, chan.voltage)
+# Create differential input between channel 0 and 1
+# chan = AnalogIn(ads, ADS.P0, ADS.P1)
+
+#csv
+file = open("/media/usb/data_log.csv", "a") 
+
+i=0 
+
+if os.stat("/home/pi/data_log.csv").st_size == 0: 
+    file.write("{:>5}\t{:>5}".format("raw", "v\n"))
+
+print("{:>5}\t{:>5}".format("raw", "v"))
 
 
+while True:
+    print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
+    file.write("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage)+"\n")
+
+    try:  # used try so that if user pressed other than the given key error will not be shown
+        if keyboard.is_pressed('s'):  # if key 's' is pressed 
+            print('******Stopped data collecting*******')
+            break  # finishing the loop
+        
+
+file.flush()
+file.close() 
