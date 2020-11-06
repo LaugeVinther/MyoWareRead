cf92c60c1bb981baef3531eb8ef444234a6b6ca3
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2020-11-06 10:39:06 +0100

    d

--- MyoWareRead/MeasureSamples.py
+++ MyoWareRead/MeasureSamples.py
@@ -15,7 +15,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Create the ADC object using the I2C bus
 ads = ADS.ADS1015(i2c)
 
-ads.data_rate = RATE
+#ads.data_rate = RATE
 
 # Create single-ended input on channel 0
 chan0 = AnalogIn(ads, ADS.P0)
