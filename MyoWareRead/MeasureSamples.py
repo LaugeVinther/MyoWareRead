7fb54d6b1ae688cd94eca28273dcda2441262215
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2020-11-06 10:38:09 +0100

    h

--- MyoWareRead/MeasureSamples.py
+++ MyoWareRead/MeasureSamples.py
@@ -6,7 +6,7 @@ from adafruit_ads1x15.ads1x15 import Mode
 from adafruit_ads1x15.analog_in import AnalogIn
 
 # Data collection setup
-RATE = 250
+RATE = 3300
 SAMPLES = 1000
 
 # Create the I2C bus with a fast frequency
@@ -15,7 +15,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Create the ADC object using the I2C bus
 ads = ADS.ADS1015(i2c)
 
-#ads.data_rate = RATE
+ads.data_rate = RATE
 
 # Create single-ended input on channel 0
 chan0 = AnalogIn(ads, ADS.P0)
