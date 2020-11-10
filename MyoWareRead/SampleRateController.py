e6fbe26e18cc17d54c52d9649854a4a03b03da73
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2020-11-10 10:33:39 +0100

    ds

--- MyoWareRead/SampleRateController.py
+++ MyoWareRead/SampleRateController.py
@@ -11,8 +11,36 @@ SAMPLES = 1000
 
 data = [None] * SAMPLES
 
+RATE = 3300
+
+# Create the I2C bus with a fast frequency
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1015(i2c)
+
+ads.mode = Mode.CONTINUOUS
+ads.data_rate = RATE
+
+# Create single-ended input on channel 0
+chan0 = AnalogIn(ads, ADS.P0)
+
+data_array = []
+
+def GetSamples():
+    while true:
+        #data_array.append(chan0.voltage)
+        data_array.append(1)
+
+
+def GetOneSample():
+    return data_array[-1]
+
+def ClearSamples():
+    data_array.clear()
+
 try:
-    _thread.start_new_thread(SC.GetSamples)
+    _thread.start_new_thread(GetSamples)
 except:
     print("Error")
 
@@ -22,7 +50,7 @@ start = time.monotonic()
 
 # Read the same channel over and over
 for i in range(SAMPLES):
-    data[i] = SC.GetOneSample()
+    data[i] = GetOneSample()
     time.sleep(0.001)
     #print(str(chan0.value))
 
