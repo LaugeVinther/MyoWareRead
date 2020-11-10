50914a65bb18fffb30fde16c44d8da8537f61bff
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2020-11-10 10:29:30 +0100

    h

--- MyoWareRead/SampleCollector.py
+++ MyoWareRead/SampleCollector.py
@@ -25,7 +25,7 @@ data_array = []
 
 def GetSamples():
     while true:
-        data_array.append(chan0.value)
+        data_array.append(chan0.voltage)
 
 def GetOneSample():
     return data_array[-1]
