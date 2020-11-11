bfba08ab57d17e0923f9abd8a51159e377df5102
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2020-11-11 11:40:04 +0100

    fdf

--- MyoWareRead/MyoWareRead.py
+++ MyoWareRead/MyoWareRead.py
@@ -8,8 +8,6 @@ import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 import keyboard
 
-SAMPLES = 1000
-
 # Create the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
 
@@ -17,24 +15,25 @@ i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1015(i2c)
 
 # Create single-ended input on channel 0
-chan = AnalogIn(ads, ADS.P0)
+chan0 = AnalogIn(ads, ADS.P0)
+chan1 = AnalogIn(ads, ADS.P1)
+chan2 = AnalogIn(ads, ADS.P2)
+chan3 = AnalogIn(ads, ADS.P3)
 
-# Create differential input between channel 0 and 1
-# chan = AnalogIn(ads, ADS.P0, ADS.P1)
+ads.data_rate = 3300
+ads.mode = Mode.CONTINUOUS
 
 
-##csv
-#f = open("/mnt/mydisk/1_no_noise.csv", "a") 
+##åben bil
+f = open("/mnt/mydisk/1.txt", "a") 
 
 
 try:
     while True:
-        print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-        #f.write(str(chan.value) + ";" + str(chan.voltage) + "\n")
-        #time.sleep(0.01)
+        f.write(str(chan0.value) + ";" + str(chan1.voltage) + ";" + str(chan2.voltage) + ";" + str(chan3.voltage) + "\n\r")
 except KeyboardInterrupt:
     pass
 
-#f.flush()
-#f.close() 
+f.flush()
+f.close() 
 print("Jeg nåede herned")
