8db34b9080c336c91dd1dc1bde88b409f9748c68
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2020-11-11 11:46:17 +0100

    d

--- MyoWareRead/MyoWareRead.py
+++ MyoWareRead/MyoWareRead.py
@@ -6,6 +6,7 @@ import board
 import busio
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+from adafruit_ads1x15.ads1x15 import Mode
 import keyboard
 
 # Create the I2C bus
