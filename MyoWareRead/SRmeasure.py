49a3c6c77741f350ccf476d0158c31a46ea85cff
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2020-12-02 13:39:20 +0100

    d

--- MyoWareRead/SRmeasure.py
+++ MyoWareRead/SRmeasure.py
@@ -33,8 +33,8 @@ start = time.monotonic()
 #print("1000 samples med brug af 'data.append(chan0.voltage)'")
 # Read the same channel over and over
 for i in range(SAMPLES):
-    #data.append(chan0.voltage)
-    data[i] = chan0.voltage
+    data.append(chan0.voltage)
+    #data[i] = chan0.voltage
     #time.sleep(0.001)
     #print(str(chan0.value))
 
